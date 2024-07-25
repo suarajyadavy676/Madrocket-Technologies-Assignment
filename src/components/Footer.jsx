fe5ed46abea97c979018c7f60ae3319609878dbe
Suraj Yadav <surajyadavy676@gmail.com>
2024-07-25 15:39:02 +0530

    remove animation on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className='flex justify-between bg-black text-white px-10 py-5 flex-wrap'data-aos="flip-up">
+    <div className='flex justify-between bg-black text-white px-10 py-5 flex-wrap'>
       <p className='my-4'>Copyright 2024 @ Springdale Public School</p>
       <p className='my-4'>Design & Developed by <a href="https://madrocket.co.in/" target='_blank' className='text-blue-500 hover:text-red-600 hover:text-lg'>MadRocket Technologies</a></p>
     </div>
